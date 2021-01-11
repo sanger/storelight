0f1caec470a81f6a33efec5254b7cc7966e2b14d
David Robinson <14000840+khelwood@users.noreply.github.com>
2021-01-11 15:30:38 +0000

    simplify apikey header

--- src/main/java/uk/ac/sanger/storelight/graphql/StorelightApi.java
+++ src/main/java/uk/ac/sanger/storelight/graphql/StorelightApi.java
@@ -4,7 +4,7 @@ package uk.ac.sanger.storelight.graphql;
  * @author dr6
  */
 public class StorelightApi {
-    public static final String API_KEY = "STORELIGHT-API-KEY";
+    public static final String API_KEY = "STORELIGHT-APIKEY";
     public static final String USER = "STORELIGHT-USER";
 
     private StorelightApi() {}
